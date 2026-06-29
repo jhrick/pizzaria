2bef95176a0c7733b894043c96404f7399beb147
hj.rickcp@gmail.com <hj.rickcp@gmail.com>
2026-06-29 11:03:03 -0300

    feat: adicionar classe Funcionario

--- src/models/Funcionario.java
+++ src/models/Funcionario.java
@@ -1,4 +1,30 @@
 package models;
 
 public class Funcionario {
+    private String nome;
+    private String cargo;
+
+    public Funcionario(String nome, String cargo) {
+        this.nome = nome;
+        this.cargo = cargo;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Função: " + cargo);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
